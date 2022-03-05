0e2616901c1019c9a708f29c047485f3e526591a
alexanderaryan <alexanderaryan.m@gmail.com>
2022-03-05 20:25:53 +0530

    Adding updated

--- apps/Paalkanakku/Paalkanakku/paalkanakku.py
+++ apps/Paalkanakku/Paalkanakku/paalkanakku.py
@@ -1,3 +1,13 @@
+#import pandas as pd
+from Paalkanakku.Paalkanakku import cred_filename, card,  file_name, gc
+
+
 import gspread
 
-gspread.authorize()
+gcc = gspread.oauth(
+    credentials_filename='/home/alexanders/Documents/Python/GitPythonWork/apps/Paalkanakku/Paalkanakku/client_secret_367285454255-v4g22iac4c6tlu0c2n5btlp7aeh2nmqp.apps.googleusercontent.com.json'
+)
+
+
+gcc.create("Test")
+
