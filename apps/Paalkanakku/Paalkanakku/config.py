059d404b91164ef620702db602f03583cfd24cf8
alexanderaryan <alexanderaryan.m@gmail.com>
2022-04-24 21:41:54 +0530

    Form control UI changed.

--- apps/Paalkanakku/Paalkanakku/config.py
+++ apps/Paalkanakku/Paalkanakku/config.py
@@ -9,10 +9,10 @@ from flask_dance.contrib.google import make_google_blueprint, google
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 
-
-cred_filename = '/home/alexanders/Documents/Python/GitPythonWork/apps/Paalkanakku/Paalkanakku/' \
+home_dir=os.path.expanduser("~")
+cred_filename = home_dir+'/Documents/Python/GitPythonWork/apps/Paalkanakku/Paalkanakku/' \
                 'client_secret_367285454255-n45ttmrb1hne9b9j4jm0ftbq6ku21qsk.apps.googleusercontent.com.json'
-client_secrets = '/home/alexanders/Documents/Python/GitPythonWork/apps/Paalkanakku/Paalkanakku/' \
+client_secrets = home_dir+'/Documents/Python/GitPythonWork/apps/Paalkanakku/Paalkanakku/' \
                 'client_secrets.json'
 
 book_name = str(datetime.today().year) + 'Paalkanakku'
@@ -28,5 +28,4 @@ google_blueprint = make_google_blueprint(client_id=google_data['client_id'],
                                          offline=True,
                                          scope=['profile', 'email'])
 
-home_dir=os.path.expanduser("~")
 sheet_config=home_dir+"/Documents/Python/GitPythonWork/apps/Paalkanakku/Paalkanakku/config.yaml"
