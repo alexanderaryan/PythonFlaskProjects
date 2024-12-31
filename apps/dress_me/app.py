5f1c544c1d0f4550df0f1d469ee2c1cb6e43cd82
alexanderaryan <alexanderaryan.m@gmail.com>
2024-12-31 23:19:55 +0530

    updated new changes

--- apps/dress_me/app.py
+++ apps/dress_me/app.py
@@ -8,5 +8,5 @@ from dressme import app,render_template,redirect
 if __name__ == '__main__':
     app.run(debug=True)
 
-#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
+#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
 
